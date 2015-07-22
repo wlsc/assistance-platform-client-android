6fd1f602833024810d5dc0d754c95deb89085c69
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-07-22 12:37:25 +0200

    * updated Snaker utils with OnClick handlers

--- app/src/main/java/de/tu_darmstadt/tk/android/assistance/utils/Snacker.java
+++ app/src/main/java/de/tu_darmstadt/tk/android/assistance/utils/Snacker.java
@@ -13,18 +13,87 @@ public class Snacker {
     }
 
     public static void showLong(View view, String message) {
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+
+        Snackbar
+                .make(view, message, Snackbar.LENGTH_LONG)
+                .show();
     }
 
     public static void showLong(View view, int resId) {
-        Snackbar.make(view, resId, Snackbar.LENGTH_LONG).show();
+        Snackbar
+                .make(view, resId, Snackbar.LENGTH_LONG)
+                .show();
+    }
+
+    public static void showLong(View view, String message, String actionText, View.OnClickListener handler) {
+
+        Snackbar
+                .make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionText, handler)
+                .show();
+    }
+
+    public static void showLong(View view, int resId, String actionText, View.OnClickListener handler) {
+
+        Snackbar
+                .make(view, resId, Snackbar.LENGTH_LONG)
+                .setAction(actionText, handler)
+                .show();
+    }
+
+    public static void showLong(View view, String message, int actionRes, View.OnClickListener handler) {
+
+        Snackbar
+                .make(view, message, Snackbar.LENGTH_LONG)
+                .setAction(actionRes, handler)
+                .show();
+    }
+
+    public static void showLong(View view, int resId, int actionRes, View.OnClickListener handler) {
+
+        Snackbar
+                .make(view, resId, Snackbar.LENGTH_LONG)
+                .setAction(actionRes, handler)
+                .show();
     }
 
     public static void showShort(View view, String message) {
-        Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
+        Snackbar
+                .make(view, message, Snackbar.LENGTH_SHORT)
+                .show();
     }
 
     public static void showShort(View view, int resId) {
-        Snackbar.make(view, resId, Snackbar.LENGTH_SHORT).show();
+        Snackbar
+                .make(view, resId, Snackbar.LENGTH_SHORT)
+                .show();
+    }
+
+    public static void showShort(View view, String message, String actionText, View.OnClickListener handler) {
+        Snackbar
+                .make(view, message, Snackbar.LENGTH_SHORT)
+                .setAction(actionText, handler)
+                .show();
+    }
+
+    public static void showShort(View view, int resId, String actionText, View.OnClickListener handler) {
+        Snackbar
+                .make(view, resId, Snackbar.LENGTH_SHORT)
+                .setAction(actionText, handler)
+                .show();
+    }
+
+    public static void showShort(View view, String message, int actionRes, View.OnClickListener handler) {
+        Snackbar
+                .make(view, message, Snackbar.LENGTH_SHORT)
+                .setAction(actionRes, handler)
+                .show();
+    }
+
+    public static void showShort(View view, int resId, int actionRes, View.OnClickListener handler) {
+        Snackbar
+                .make(view, resId, Snackbar.LENGTH_SHORT)
+                .setAction(actionRes, handler)
+                .show();
     }
 }
