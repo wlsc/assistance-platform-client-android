1b5d6b0d835cbe5121a3555bac6f6729f8e4ef06
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-09-07 20:44:41 +0200

    * updated gradle to 2.6

--- app/src/main/java/de/tu_darmstadt/tk/android/assistance/adapter/DrawerAdapter.java
+++ app/src/main/java/de/tu_darmstadt/tk/android/assistance/adapter/DrawerAdapter.java
@@ -2,7 +2,6 @@ package de.tu_darmstadt.tk.android.assistance.adapter;
 
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
