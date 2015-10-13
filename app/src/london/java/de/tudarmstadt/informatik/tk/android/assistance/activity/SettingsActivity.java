149ef021f130b469d3f6bc3e73bc2fb0f2917fca
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-10-13 11:31:43 +0200

    * exchanged logout flow with EventBus

--- app/src/london/java/de/tudarmstadt/informatik/tk/android/assistance/activity/SettingsActivity.java
+++ app/src/london/java/de/tudarmstadt/informatik/tk/android/assistance/activity/SettingsActivity.java
@@ -13,9 +13,10 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import de.greenrobot.event.EventBus;
 import de.tudarmstadt.informatik.tk.android.assistance.R;
 import de.tudarmstadt.informatik.tk.android.assistance.util.PreferencesUtils;
-import de.tudarmstadt.informatik.tk.android.kraken.provider.HarvesterServiceProvider;
+import de.tudarmstadt.informatik.tk.android.kraken.event.StopSensingEvent;
 
 /**
  * Core user settings activity
@@ -112,23 +113,16 @@ public class SettingsActivity extends PreferenceActivity {
      */
     private void doLogout() {
 
+        Log.d(TAG, "Doing logout...");
+
         PreferencesUtils.clearUserCredentials(getApplicationContext());
 
         setResult(R.id.logout_settings);
 
         // stop the kraken
-        stopSensingService();
+        EventBus.getDefault().post(new StopSensingEvent(getApplicationContext()));
 
         finish();
-    }
-
-    /**
-     * Calms down the Kraken.
-     */
-    private void stopSensingService() {
-
-        HarvesterServiceProvider service = HarvesterServiceProvider.getInstance(getApplicationContext());
-        service.stopService();
     }
 
     public Toolbar getToolBar() {
