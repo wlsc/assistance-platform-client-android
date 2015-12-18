0cb882286559047e3e06f19307f23f0037a66c40
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-12-18 23:35:50 +0100

    + added mapping between some items from client module feedback channel and json2view library + added test for valid mapping of type "text"

---
+++ app/src/androidTest/java/de/tudarmstadt/informatik/tk/android/assistance/util/JsonUtilsTest.java
@@ -0,0 +1,69 @@
+package de.tudarmstadt.informatik.tk.android.assistance.util;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import de.tudarmstadt.informatik.tk.assistance.model.client.feedback.ContentDto;
+import de.tudarmstadt.informatik.tk.assistance.model.client.feedback.enums.FeedbackItemType;
+import de.tudarmstadt.informatik.tk.assistance.model.client.feedback.enums.TextAlignment;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Wladimir Schmidt (wlsc.dev@gmail.com)
+ * @date 17.12.2015
+ */
+@RunWith(AndroidJUnit4.class)
+public class JsonUtilsTest {
+
+    private Gson gson;
+
+    public JsonUtilsTest() {
+        gson = new Gson();
+    }
+
+    @Before
+    public void before() throws Exception {
+
+    }
+
+    @After
+    public void after() throws Exception {
+
+    }
+
+    @Test
+    public void testJson2viewFormatIsOK() {
+
+        JsonObject jsonObjectShould = new JsonObject();
+        jsonObjectShould.addProperty("widget", "TextView");
+        jsonObjectShould.add("properties", new JsonArray());
+
+        ContentDto resource = new ContentDto();
+
+        resource.setType(FeedbackItemType.TEXT.getValue());
+        resource.setCaption("test");
+        resource.setStyle(new String[]{});
+        resource.setHighlighted(Boolean.FALSE);
+        resource.setTextAlignment(TextAlignment.LEFT.getValue());
+
+        JsonParser gsonParser = new JsonParser();
+        JsonObject gsonJsonObject = gsonParser.parse(gson.toJson(resource)).getAsJsonObject();
+
+        JsonObject json2ViewFormat = JsonUtils.mapFeedbackToJson2ViewFormat(gsonJsonObject);
+
+        assertNotNull(json2ViewFormat.get("widget"));
+        assertThat(jsonObjectShould.get("widget").getAsString(), is(json2ViewFormat.get("widget").getAsString()));
+    }
+}
---
+++ app/src/main/java/de/tudarmstadt/informatik/tk/android/assistance/util/JsonUtils.java
@@ -0,0 +1,215 @@
+package de.tudarmstadt.informatik.tk.android.assistance.util;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+import de.tudarmstadt.informatik.tk.assistance.model.client.feedback.enums.FeedbackItemType;
+
+/**
+ * @author Wladimir Schmidt (wlsc.dev@gmail.com)
+ * @date 17.12.2015
+ */
+public class JsonUtils {
+
+    private static final String TAG = JsonUtils.class.getSimpleName();
+
+    private JsonUtils() {
+    }
+
+    /**
+     * Mapping of an object received from server as feedback for modules
+     * into a valid json2view library format
+     *
+     * @param oldObject
+     * @return
+     */
+    public static JsonObject mapFeedbackToJson2ViewFormat(JsonObject oldObject)
+            throws ClassCastException, IllegalStateException {
+
+        if (oldObject == null) {
+            return null;
+        }
+
+        JsonObject newObject = null;
+
+        String contentTypeJson = oldObject.get("type").getAsString();
+        FeedbackItemType contentType = FeedbackItemType.getEnum(contentTypeJson);
+
+        // mapping content to the right format
+        switch (contentType) {
+            case BUTTON:
+                newObject = formatButtonObject(oldObject);
+                break;
+            case TEXT:
+                newObject = formatTextObject(oldObject);
+                break;
+            case IMAGE:
+                newObject = formatImageObject(oldObject);
+                break;
+            default:
+                newObject = null;
+                break;
+        }
+
+        return newObject;
+    }
+
+    /**
+     * Formats group object into json2view format
+     * JSON example:
+     * "caption": "test",
+     * "highlighted": false,
+     * "style": [],
+     * "textAlignment": "LEFT",
+     * "type": "text"
+     *
+     * @param oldObject
+     * @return
+     */
+    private static JsonObject formatGroupObject(JsonObject oldObject) {
+
+        JsonObject newObject = null;
+
+        return newObject;
+    }
+
+    /**
+     * Formats image object into json2view format
+     * JSON example:
+     * "source": "img url",
+     * "target": "url/app",
+     * "priority": 1,
+     * "type": "image"
+     *
+     * @param oldObject
+     * @return
+     */
+    private static JsonObject formatImageObject(JsonObject oldObject) {
+
+        if (oldObject == null) {
+            return null;
+        }
+
+        JsonArray properties = new JsonArray();
+
+        JsonObject newObject = new JsonObject();
+        newObject.addProperty("widget", "ImageView");
+        newObject.add("properties", properties);
+
+        return newObject;
+    }
+
+    /**
+     * Formats button object into json2view format
+     * JSON example:
+     * "caption": "test",
+     * "target": "url/app",
+     * "priority": 1,
+     * "type": "button"
+     *
+     * @param oldObject
+     * @return
+     */
+    private static JsonObject formatButtonObject(JsonObject oldObject)
+            throws ClassCastException, IllegalStateException {
+
+        if (oldObject == null) {
+            return null;
+        }
+
+        String caption = oldObject.get("caption").getAsString();
+
+        JsonArray properties = new JsonArray();
+
+        JsonObject captionObject = new JsonObject();
+        captionObject.addProperty("name", "id");
+        captionObject.addProperty("type", "string");
+        captionObject.addProperty("value", caption);
+
+        properties.add(captionObject);
+
+        JsonObject newObject = new JsonObject();
+        newObject.addProperty("widget", "Button");
+        newObject.add("properties", properties);
+
+        return newObject;
+    }
+
+    /**
+     * Formats text object into json2view format
+     * JSON example:
+     * "caption": "test",
+     * "highlighted": false,
+     * "style": [],
+     * "textAlignment": "LEFT",
+     * "type": "text"
+     *
+     * @param oldObject
+     * @return
+     */
+    private static JsonObject formatTextObject(JsonObject oldObject) {
+
+        if (oldObject == null) {
+            return null;
+        }
+
+        String caption = oldObject.get("caption").getAsString();
+        boolean highlighted = oldObject.get("highlighted").getAsBoolean();
+        String alignment = oldObject.get("textAlignment").getAsString();
+
+        JsonArray properties = new JsonArray();
+
+        JsonObject captionObject = new JsonObject();
+        captionObject.addProperty("name", "id");
+        captionObject.addProperty("type", "string");
+        captionObject.addProperty("value", caption);
+
+        properties.add(captionObject);
+
+        if (highlighted) {
+
+            JsonObject highlightedObject = new JsonObject();
+
+            highlightedObject.addProperty("name", "textColor");
+            highlightedObject.addProperty("type", "color");
+            highlightedObject.addProperty("value", "0xFF0000");
+
+            properties.add(highlightedObject);
+        }
+
+        JsonObject alignmentObject = new JsonObject();
+        alignmentObject.addProperty("name", "gravity");
+        alignmentObject.addProperty("type", "string");
+        alignmentObject.addProperty("value", alignment.toUpperCase());
+
+        properties.add(alignmentObject);
+
+        JsonObject newObject = new JsonObject();
+        newObject.addProperty("widget", "TextView");
+        newObject.add("properties", properties);
+
+        return newObject;
+    }
+
+    /**
+     * Checks for valid JSON string with GSON library
+     *
+     * @param json
+     * @return
+     */
+
+    public static boolean isValidJSON(String json) {
+
+        try {
+
+            Gson gson = new Gson();
+            gson.fromJson(json, Object.class);
+            return true;
+
+        } catch (JsonSyntaxException e) {
+            return false;
+        }
+    }
+}
